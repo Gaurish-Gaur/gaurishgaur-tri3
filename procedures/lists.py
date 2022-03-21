f469b24cc2eccf2650bcdc5e2f9c467309b290af
Gaurish-Gaur <89219754+Gaurish-Gaur@users.noreply.github.com>
2022-03-21 21:21:05 +0000

    Final touches

--- procedures/lists.py
+++ procedures/lists.py
@@ -1,5 +1,6 @@
 InfoDb = []
 # List with dictionary records placed in a list  
+#List includes many indicators, like FirstName or Balance. Balance could mean amount of credu one had and is ready to spend. As in, do they have the balance to spend at perhaps a more expensive restraunt?
 InfoDb.append({  
                "FirstName": "Gaurish",  
                "LastName": "Gaur",  
@@ -26,6 +27,14 @@ InfoDb.append({
                "Fav_Restraunts":["Chick-fil-A", "Gorgai", "Board and Brew"],
                "Least_Fav_Restraunts":["Daphnies", "Jack-in-the-box"]
               })  
+InfoDb.append({  
+               "FirstName": "Connor",  
+               "LastName": "Wang",  
+               "Balance": "-50",  
+               "Email": "connorwang2@gmail.com",  
+               "Fav_Restraunts":["Chipolte", "Panda Express", "Board and Brew"],
+               "Least_Fav_Restraunts":["IHop", "Luna Grill"]
+              })  
 
 #print(InfoDb[0]["Fav_Restraunts"][1])
 #print(InfoDb[1]["Fav_Restraunts"][0])
@@ -36,23 +45,25 @@ def print_data(n):
     print(", ".join(InfoDb[n]["Fav_Restraunts"]))  # join allows printing a string list with separator
     print()
 
+#self explanatory procedure. Getting certain datat with few guidelines on format like commas and spaces to neaten up. n creates itteration.
   
 def for_loop():
     for n in range(len(InfoDb)):
         print_data(n)
-
+#for loop i shown as seen with "for"
 def while_loop(n):
     while n < len(InfoDb):
         print_data(n)
         n += 1
     return
+#While loop is shown as seen with "while"
 
 def recursive_loop(n):
     if n < len(InfoDb):
         print_data(n)
         recursive_loop(n + 1)
     return 
-
+#recursive since we keep checking condition, and in the mean time continue to print. And once that codition is met, and we have printed each part of the list, we can return and leave the process
 
 
 def tester():
